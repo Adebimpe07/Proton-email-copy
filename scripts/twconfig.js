c76e69e36c13d2b4e0cd306431e55d9b23c7370c
Adebimpe07 <100203234+Adebimpe07@users.noreply.github.com>
2022-09-01 09:30:46 +0100

    proton project

---
+++ scripts/twconfig.js
@@ -0,0 +1,26 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      colors: {
+        dpurple: "#4c34b3",
+        ddpurple: "#160f33",
+        primary: "#1b1340",
+        oprimary: "#4A368F4D",
+        bottom_border: "#d1cfcd",
+        purple_1: "#6d4aff",
+        purple_2: "#7C5CFF",
+        purple_3: "#9282D4",
+        purple_4: "#6243e6",
+        purple_5: "#8a6eff",
+        purple_6: "#38277A",
+        purple_white: "#DED7FF",
+        yellow: "#ff9900",
+        hover_1: "#dedbd9",
+        hover_2: "#e5e4e1",
+      },
+      fontFamily: {
+        sans: ["system-ui", "sans-serif"],
+      },
+    },
+  },
+};
